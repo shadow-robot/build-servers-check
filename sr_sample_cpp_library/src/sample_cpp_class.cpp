7166f294e0f80d54b6cbf3c0402fcc14e27dfa0c
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-10-13 12:46:32 +0300

    Removed previous commits

--- sr_sample_cpp_library/src/sample_cpp_class.cpp
+++ sr_sample_cpp_library/src/sample_cpp_class.cpp
@@ -34,9 +34,6 @@ std::string SampleCppClass::getParameterValueFromName(const std::string &paramet
     std::string result = parameter_name;
     result.append("_test");
     // small changes
-    // small changes again
-    // small changes again 2
-    // small changes again 3
     return result;
 }
 
