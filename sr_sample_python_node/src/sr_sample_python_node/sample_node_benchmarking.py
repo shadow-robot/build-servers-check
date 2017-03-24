cfc5e99768b7cfdda98ce445d057cb11b455fede
Build Tools <build.tools@example.com>
2017-03-24 08:43:59 +0000

    Fixed benchmarking configuration

--- sr_sample_python_node/src/sr_sample_python_node/sample_node_benchmarking.py
+++ sr_sample_python_node/src/sr_sample_python_node/sample_node_benchmarking.py
@@ -3,26 +3,29 @@
 See README.md
 """
 import rospy
-from sr_benchmarking.srv import ExecuteBenchmarking, ExecuteBenchmarkingResponse
+from sr_benchmarking_msgs.srv import ExecuteBenchmarking, ExecuteBenchmarkingResponse
 import time
 
 
 def execute_benchmarking(request):
 
     start = time.time()
-    param_value = rospy.get_param('my_python_param')
     while not rospy.is_shutdown():
+        param_value = rospy.get_param('my_python_param')
         if 'my_python_param_test' != param_value:
             rospy.sleep(0.1)
+        else:
+            break
 
     end = time.time()
     time_spend = end - start
 
     with open(request.output_file, 'w') as file:
-        file.write('result,' + time_spend)
+        file.write('result,' + str(time_spend))
     return ExecuteBenchmarkingResponse(True)
 
 if __name__ == "__main__":
+
     rospy.init_node('sample_node_benchmarking')
     _ = rospy.Service('execute_benchmarking', ExecuteBenchmarking, execute_benchmarking)
     rospy.spin()
