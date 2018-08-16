c0faae6dc36a707b6ee2999291b4c2c9260143cc
Andriy Petlovanyy <andriy.petlovanyy@noemail.com>
2018-08-16 16:38:44 +0000

    Fixed build

--- sr_sample_python_node/src/sr_sample_python_node/sample_python_node.py
+++ sr_sample_python_node/src/sr_sample_python_node/sample_python_node.py
@@ -6,14 +6,11 @@ See README.md
 import rospy
 from sr_sample_python_library.sample_python_class import SamplePythonClass
 
-
-
-
 if __name__ == "__main__":
 
     rospy.init_node("sample_python_node")
 
-    sample_object   =   SamplePythonClass()
+    sample_object = SamplePythonClass()
 
     param_name = "my_python_param"
     param_value = sample_object.get_parameter_name_from_value(param_name)
